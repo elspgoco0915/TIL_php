0e3953663cf7a4f5435ec0856ac96541ec8d3f01
elspgoco0915 <elspgoco0915@gmail.com>
2024-03-28 18:05:54 +0900

    #3 パーフェクトphp用のディレクトリの作成

---
+++ src_perfect_php/app/Web/index.php
@@ -0,0 +1,4 @@
+<?php
+
+require '../../bootstrap/bootstrap.php';
+// TODO: 23.03.28 次回
---
+++ src_perfect_php/app/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+// NOTE: feature/#3 perfect php
+require __PROJECT_ROOT__  . "/app/Core/ClassLoader.php";
+
+$loader = new ClassLoader();
+$loader->registerDir(__PROJECT_ROOT__.'/app/Core');
+$loader->registerDir(__PROJECT_ROOT__.'/app/Models');
+$loader->register();
+
+// TODO: 後で消す
+echo "execute classLoader!!";
