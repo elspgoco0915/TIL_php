8cc5b6791e29c5421edaea8fd97eb0692dfd7b3f
elspgoco0915 <elspgoco0915@gmail.com>
2024-04-17 07:06:59 +0900

    #7 ビット演算を用いたフラグ管理方法、プログラムのみ　の修正

--- src/app/Enums/StatusItem.php
+++ src/app/Enums/StatusItem.php
@@ -13,11 +13,11 @@ enum StatusItem: int
     // NOTE: 0x = 16進数
 
     // 不死鳥の羽 ... 死亡から回復
-    const FUSHICHONOO = 0b000001;
+    case FUSHICHONOO = 0b000001;
     // 毒消し ... 毒から回復
-    const DOKUKESHI   = 0b000010;
+    case DOKUKESHI   = 0b000010;
     // 眠眠打破 ... 眠りから回復
-    const MINMINDAHA  = 0b000100;
+    case MINMINDAHA  = 0b000100;
     // 万能薬 ... 死亡以外の状態上から回復
-    const BANNOUYAKU  = 0b111110;
+    case BANNOUYAKU  = 0b111110;
 }
