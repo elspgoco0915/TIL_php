2b2a1d0bf79491b32f34b1a54f2da2e158734207
elspgoco0915 <elspgoco0915@gmail.com>
2024-04-17 07:13:11 +0900

    #7 ビット演算を用いたフラグ管理方法、プログラム版

--- src/app/Service/BitmaskService.php
+++ src/app/Service/BitmaskService.php
@@ -16,37 +16,22 @@ class BitMaskService {
 
         // 初期状態は状態異常なし
         $player = new Player();
-        echo decbin($player->getState())."<br>";
-        
-        // 毒を追加
-        $player->setState(PlayerStatus::POISON->value);
-        echo decbin($player->getState())."<br>";
-        // 麻痺を追加したいが、毒がなくなり上書きされてしまう
-        $player->setState(PlayerStatus::PARALYSIS->value);
-        echo decbin($player->getState())."<br>";
+        echo $player->getState()."<br>";
 
-        // ビット演算子を使用することで、解決
-        $player->setState(PlayerStatus::POISON->value);
-        // ビット和
-        $player->setState($player->state | PlayerStatus::PARALYSIS->value);
-        echo decbin($player->getState())."<br>";
-        // さらに追加することで、「毒、まひ、混乱」が上書きされず追加できた。
-        $player->setState($player->state | PlayerStatus::CONFUSION->value);
-        echo decbin($player->getState())."<br>";
-        
-        // 回復
-        // ビット演算子のビット積(&)と否定(~)を使用する
-        // ビット積＝右辺左辺の両方にセットされている場合、ビットがセットされる
-        // 否定=セットされているビットの逆になる　0b111110 -> 0b000001
-        // 0b111110 & 0b000001 = 0b000000
-        $player->setState($player->state & ~StatusItem::BANNOUYAKU);
-        // よって、状態異常が回復する
-        echo decbin($player->getState())."<br>";
-    
+        $player->setState(PlayerStatus::POISON);
+        echo $player->getState()."<br>";
+
+        $player->setState(PlayerStatus::PARALYSIS);
+        echo $player->getState()."<br>";
+
+        $player->setState(PlayerStatus::CONFUSION);
+        echo $player->getState()."<br>";
+
+        $player->removeState(StatusItem::BANNOUYAKU);
+        echo $player->getState()."<br>";
+
         echo "<hr>";
         exit;
-        // memo
-        // 「&（論理積・AND）」、「|（論理和OR）」「^（排他的論理和XOR)」を
 
     }
 }
